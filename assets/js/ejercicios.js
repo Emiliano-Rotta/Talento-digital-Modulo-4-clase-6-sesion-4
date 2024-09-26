1e849f7b8d9abb91d6fccf796b83dffaaa9d9f0c
Emiliano Rotta <emilianorottaf@gmail.com>
2024-09-26 20:20:37 -0300

    ejercicios

---
+++ assets/js/ejercicios.js
@@ -0,0 +1,22 @@
+//Ejercicios individuales:
+
+//20.20 a 20.40 recreo
+//20.40 a 21.05 ejercicios para hacer
+
+// Ejercicio 1: Combinar Objetos con Spread y Valores por Defecto
+// Consigna:
+// Tienes un objeto usuario que contiene información básica: usuario = { nombre: 'Luis', edad: 30 }. También tienes otro objeto datosAdicionales que puede contener información adicional: datosAdicionales = { ciudad: 'Valencia', profesion: 'Desarrollador' }. Utiliza el operador Spread para combinar ambos objetos en un nuevo objeto llamado perfilCompleto, asegurándote de que si datosAdicionales no incluye profesion, se establezca un valor por defecto como 'Desconocido'. Muestra el objeto resultante en la consola.
+
+//-----------------------------------------
+
+// Ejercicio 2: Clonación y Modificación de Objetos
+// Consigna:
+// Tienes un objeto producto que describe un producto en una tienda: producto = { id: 1, nombre: 'Laptop', precio: 1000 }. Usa el operador Spread para crear un nuevo objeto productoConDescuento que contenga la misma información pero con un precio reducido en un 20%. Asegúrate de que el objeto original no se modifique. Muestra ambos objetos en la consola. tips: para reducir un 20%, hay que multiplicar el valor por 0.8
+
+
+//----------------------------------------------
+
+// Ejercicio 3: Fusionar y Ordenar Arrays
+// Consigna:
+// Tienes tres arrays de números: arrayA = [5, 10, 15], arrayB = [3, 6, 9], y arrayC = [12, 0, 8]. Utiliza el operador Spread para combinar todos estos arrays en un solo array arrayFinal, y luego ordénalo en orden ascendente. Muestra el array ordenado en la consola.
+// tips: para ordenar usar el metodo sort
